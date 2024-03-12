5fe57aefeabc5785b93f7ce755552e5a9e083166
Antonio Manuel Pérez López <aperezl@gmail.com>
2024-03-12 13:32:43 +0100

    chore: add chat endpoint

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -4,23 +4,45 @@ import { StreamingTextResponse, Message } from 'ai'
 import { AIMessage, HumanMessage } from "@langchain/core/messages";
 import model from "@/lib/llms/llm"
 import { BytesOutputParser } from "@langchain/core/output_parsers";
+import { ChatPromptTemplate } from '@langchain/core/prompts'
+import { makeTemplateWithMessages } from '@/lib/prompts/templates';
+import { MemoryVectorStore } from 'langchain/vectorstores/memory';
+import { createVectorStore, createChain } from '@/chat/chatWithHistory';
 
 export const runtime = "edge";
 
 export async function POST(req: Request) {
-    const { messages } = await req.json();
+  console.log('init')
+
+  const { messages } = await req.json();
+  console.log(messages)
+  const vectorStore = await createVectorStore()
+  console.log('create_vector')
+
+  const chain = await createChain(vectorStore)
+  console.log('create_chain')
+
+  const chatHistory = messages
+
+  const response = await chain.invoke({
+    input: chatHistory,
+    // chat_history: chatHistory
+  })
+
+  console.log(response)
   
-    const parser = new BytesOutputParser();
+    // const parser = new BytesOutputParser();
+    // // const chain = prompt.pipe(model).invoke({ input:  })
   
-    const stream = await model
-      .pipe(parser)
-      .stream(
-        (messages as Message[]).map((m) =>
-          m.role == "user"
-            ? new HumanMessage(m.content)
-            : new AIMessage(m.content)
-        )
-      );
+    // const stream = await prompt.pipe(model)
+    //   .pipe(parser)
+    //   .stream(
+    //     (messages as Message[]).map((m) =>
+    //       m.role == "user"
+    //         ? new HumanMessage(m.content)
+    //         : new AIMessage(m.content)
+    //     )
+    //   );
   
-    return new StreamingTextResponse(stream);
-  }
+    // return new StreamingTextResponse(stream);
+}
