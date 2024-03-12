fbddc38c143635a3c68e0c3ddfc50cf98da18ca5
aperezl <aperezl@gmail.com>
2024-03-12 08:09:01 +0100

    feat: init frontend

--- src/app/api/basic/stream/route.ts
+++ src/app/api/basic/stream/route.ts
@@ -1,9 +1,14 @@
 import { NextRequest } from 'next/server'
 import { StreamingTextResponse } from 'ai'
+import { BytesOutputParser } from '@langchain/core/output_parsers'
+
 import model from '@/llms/llm'
 
 export const GET = async (req: NextRequest) => {
   const message = req.nextUrl.searchParams.get('message') as string
-  const response = await model.stream(message ?? 'Hello')
-  return new StreamingTextResponse(response)
+  const stream = await model
+    .pipe(new BytesOutputParser())
+    .stream(message ?? 'Hello')
+
+  return new StreamingTextResponse(stream)
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 
 export default function Home () {
   return (
     <div>
+      <Button>Click me</Button>
       <h1 className='text-3xl'>Basic</h1>
       <ul>
         <li>
